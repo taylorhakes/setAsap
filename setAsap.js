d75b7245ba4a4905ee284f91423a5aeda8625d7f
Taylor Hakes <taylor@taylorhakes.com>
2015-01-08 21:54:57 -0500

    used the same callback for all functions

--- setAsap.js
+++ setAsap.js
@@ -2,65 +2,58 @@
 	'use strict';
 	var setAsap = (function () {
 		var callbacks = [], timeout, hiddenDiv, scriptEl, timeoutFn;
-		if(typeof window !== 'undefined') main = window;
+		if (typeof window !== 'undefined') main = window;
 		// Modern browsers, fastest async
 		if (main.MutationObserver) {
 			hiddenDiv = document.createElement("div");
 			(new MutationObserver(executeCallbacks)).observe(hiddenDiv, { attributes: true });
-			return function (callback) {
-				if (!callbacks.length) {
-					hiddenDiv.setAttribute('yes', 'no');
-				}
-				callbacks.push(callback);
-			};
-		// Browsers that support postMessage
+			return getAsap(function () {
+				hiddenDiv.setAttribute('yes', 'no');
+			});
+			// Browsers that support postMessage
 		} else if (!main.setImmediate && main.postMessage && !main.importScripts && main.addEventListener) {
 			var MESSAGE_PREFIX = "com.setImmediate" + Math.random(), hasPostMessage = false;
 
 			var onGlobalMessage = function (event) {
 				if (event.source === main && event.data === MESSAGE_PREFIX) {
-					hasPostMessage = false;
 					executeCallbacks();
 				}
 			};
 
 			main.addEventListener("message", onGlobalMessage, false);
 
-			return function (fn) {
-				callbacks.push(fn);
-
-				if (!hasPostMessage) {
-					hasPostMessage = true;
-					main.postMessage(MESSAGE_PREFIX, "*");
-				}
-			};
-		// IE browsers without postMessage
+			return getAsap(function () {
+				main.postMessage(MESSAGE_PREFIX, "*");
+			});
+			// IE browsers without postMessage
 		} else if (!main.setImmediate && main.document && 'onreadystatechange' in document.createElement('script')) {
-			return function (callback) {
-				callbacks.push(callback);
-				if (!scriptEl) {
-					scriptEl = document.createElement("script");
-					scriptEl.onreadystatechange = function () {
-						scriptEl.onreadystatechange = null;
-						scriptEl.parentNode.removeChild(scriptEl);
-						scriptEl = null;
-						executeCallbacks();
-					};
-					document.body.appendChild(scriptEl);
-				}
-			};
-		// All other browsers and node
+			return getAsap(function () {
+				scriptEl = document.createElement("script");
+				scriptEl.onreadystatechange = function () {
+					scriptEl.onreadystatechange = null;
+					scriptEl.parentNode.removeChild(scriptEl);
+					scriptEl = null;
+					executeCallbacks();
+				};
+				document.body.appendChild(scriptEl);
+			});
+			// All other browsers and node
 		} else {
 			timeoutFn = main.setImmediate || (main.process && main.process.nextTick) || setTimeout;
+			return getAsap(function () {
+				timeoutFn(function () {
+					executeCallbacks();
+				}, 0);
+			});
+		}
+
+		function getAsap(fn) {
 			return function (callback) {
-				callbacks.push(callback);
-				if (!timeout) {
-					timeout = timeoutFn(function () {
-						timeout = undefined;
-						executeCallbacks();
-					}, 0);
+				if (!callback.length) {
+					fn();
 				}
-			};
+				callbacks.push(callback);
+			}
 		}
 
 		function executeCallbacks() {
@@ -81,5 +74,4 @@
 	} else {
 		main.setAsap = setAsap;
 	}
-
 })(this);
