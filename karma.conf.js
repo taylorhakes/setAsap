5fca85e9c8ceebbe2d30ff3f2488753bfe82142d
Taylor Hakes <taylorhakes@users.noreply.github.com>
2018-11-22 09:45:25 -0500

    Moved from PhantomJS to Chrome headless

--- karma.conf.js
+++ karma.conf.js
@@ -49,7 +49,7 @@ module.exports = function (config) {
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ['PhantomJS']/*Object.keys(customLaunchers)*/,
+		browsers: ['ChromeHeadless']/*Object.keys(customLaunchers)*/,
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
